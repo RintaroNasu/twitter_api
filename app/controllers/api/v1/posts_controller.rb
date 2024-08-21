e7015bd23a33bd29962026f0fc3fb7ff8e7cd7d0
RintaroNasu <rinta022823@gmail.com>
2024-08-21 19:08:48 +0900

    検索機能

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -4,6 +4,18 @@ class Api::V1::PostsController < ApplicationController
     render json: @posts
   end
 
+  def search
+    query = params[:query]
+    results = Post.where(content: query)
+    puts "result: #{results.to_a.inspect}"
+    if results
+      render json: results
+    else
+      render json:result.errors
+    end
+
+  end
+
   def show
     @post = Post.find(params[:id])
     render json: @post
@@ -32,6 +44,7 @@ class Api::V1::PostsController < ApplicationController
     @post = Post.find(params[:id])
     @post.destroy
   end
+
   private 
     def posts_params
       params.require(:post).permit(:content)
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :posts, only: [:index,:create,:show, :update, :destroy]
+      resources :posts, only: [:index,:create,:show, :update, :destroy,:search] do
+        collection do
+          get 'search'
+        end
+      end
     end
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
